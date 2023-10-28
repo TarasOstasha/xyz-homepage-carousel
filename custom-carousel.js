32464e768024a6a487b0e4980f5b8e5ed9882ed7
Taras <tonyjoss1990@gmail.com>
2023-10-28 13:19:55 -0400

    smooth carousel add

---
+++ custom-carousel.js
@@ -0,0 +1,381 @@
+$carouselData = [
+  {
+    title: 'Backlit Displays',
+    path:
+      'https://www.xyzdisplays.com/Backlit-Trade-Show-Displays-and-Backdrops-s/22964.htm',
+    imgFront:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/mk90730-2T.png?v-cache=1670494018',
+    imgBack:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/MK70180-2T.jpg?v-cache=1671610996',
+  },
+  {
+    title: 'Towers',
+    path:
+      'https://www.xyzdisplays.com/Tower-Displays-and-Columns-for-Trade-Shows-s/275.htm',
+    imgFront:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/or100350-2T.png?v-cache=1680868235',
+    imgBack:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/ws51960-2T.jpg?v-cache=1670494018',
+  },
+  {
+    title: 'Digital Kiosks',
+    path:
+      'https://www.xyzdisplays.com/Touch-Screen-Kiosks-and-Digital-Signage-s/1604.htm',
+    imgFront:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/LS10510-2T.jpg?v-cache=1670494018',
+    imgBack:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/LS10400-2T.jpg?v-cache=1670494018',
+  },
+  {
+    title: 'Hanging Signs',
+    path:
+      'https://www.xyzdisplays.com/Trade-Show-Hanging-Banner-Displays-Signs-s/86.htm',
+    imgFront:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/mk30002-2T.jpg?v-cache=1670494018',
+    imgBack:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/or41542-2T.jpg?v-cache=1670494018',
+  },
+  {
+    title: 'Flooring',
+    path: 'https://www.xyzdisplays.com/category-s/46048.htm',
+    imgFront:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/ks120-2T.jpg?v-cache=1678874912',
+    imgBack:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/ks200-2T.jpg?v-cache=1679645937',
+  },
+
+  {
+    title: 'Counters',
+    path:
+      'https://www.xyzdisplays.com/Trade-Show-Counters-and-Podiums-s/159.htm',
+    imgFront:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/ws04220-2T.png?v-cache=1670494018',
+    imgBack:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/na990-2T.png?v-cache=1670494018',
+  },
+  {
+    title: 'Backlit Towers',
+    path:
+      'https://www.xyzdisplays.com/Illuminated-Tension-Fabric-Backlit-Towers-Displays-s/1610.htm',
+    imgFront:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/mk70850-2T.jpg?v-cache=1670494018',
+    imgBack:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/or100380-2T.jpg?v-cache=1680868305',
+  },
+  {
+    title: 'Turntables',
+    path:
+      'https://www.xyzdisplays.com/Motorized-Rotating-Display-Turntable-s/1309.htm',
+    imgFront:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/dc00421-2T.jpg?v-cache=1670494018',
+    imgBack:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/dc00160-2T.jpg?v-cache=1670494018',
+  },
+  {
+    title: 'Canopy Tents',
+    path:
+      'https://www.xyzdisplays.com/Outdoor-Tents-Flags-and-Banner-Stands-s/187.htm',
+    imgFront:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/ws20400m-2T.jpg?v-cache=1670494018',
+    imgBack:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/OR29080-2T.jpg?v-cache=1670494018',
+  },
+  {
+    title: 'Popup Displays',
+    path:
+      'https://www.xyzdisplays.com/Pop-Up-Tension-Fabric-Displays-s/24605.htm',
+    imgFront:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/ws04852m-2T.jpg?v-cache=1670494018',
+    imgBack:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/or56230-2T.jpg?v-cache=1670494018',
+  },
+  {
+    title: 'SEG Displays',
+    path:
+      'https://www.xyzdisplays.com/SEG-System-Tension-Fabric-Displays-s/1653.htm',
+    imgFront:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/or70040-3T.png?v-cache=1682921753',
+    imgBack:
+      'https://cdn3.volusion.com/wgjfq.aujvw/v/vspfiles/photos/ws54300-2T.jpg?v-cache=1681463667',
+  },
+  {
+    title: 'Table Throws',
+    path:
+      'https://www.xyzdisplays.com/Trade-Show-Table-Throws-Covers-and-Runners-s/151.htm',
+    imgFront:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/or89820-2T.jpg?v-cache=1670494018',
+    imgBack:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/ws05510-2T.jpg?v-cache=1670494018',
+  },
+  {
+    title: 'TV Stands',
+    path: 'https://www.xyzdisplays.com/TV-Stands-and-Kiosks-s/31137.htm',
+    imgFront:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/or27240-2T.png?v-cache=1670494018',
+    imgBack:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/or27150-2T.jpg?v-cache=1670494018',
+  },
+  {
+    title: 'Cases',
+    path:
+      'https://www.xyzdisplays.com/Hard-Shipping-Cases-and-Carry-Bags-s/161.htm',
+    imgFront:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/or66305-2T.jpg?v-cache=1670494018',
+    imgBack:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/or6606-4T.jpg?v-cache=1670494018',
+  },
+  {
+    title: 'Blimp Towers',
+    path: 'https://www.xyzdisplays.com/Blimp-Tower-Displays-s/10708.htm',
+    imgFront:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/MK34600-2T.jpg?v-cache=1670494018',
+    imgBack:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/or63850-2T.jpg?v-cache=1678446186',
+  },
+  {
+    title: 'Custom Arches',
+    path:
+      'https://www.xyzdisplays.com/Formulate-Arches-and-Conference-Walls-s/10709.htm',
+    imgFront:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/bn00200-2T.png?v-cache=1670494018',
+    imgBack:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/or36510-2T.jpg?v-cache=1670494018',
+  },
+  {
+    title: 'Truss Exhibits',
+    path:
+      'https://www.xyzdisplays.com/Orbital-Truss-Trade-Show-Exhibits-s/141.htm',
+    imgFront:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/OR0672-3T.jpg?v-cache=1670494018',
+    imgBack:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/or1100-3T.jpg?v-cache=1670494018',
+  },
+  {
+    title: 'Step & Repeat Backdrops',
+    path: 'https://www.xyzdisplays.com/Step-Repeat-Banner-Displays-s/11508.htm',
+    imgFront:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/bs1110-2T.jpg?v-cache=1674732952',
+    imgBack:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/bs1110-3.jpg?v-cache=1674732952',
+  },
+  {
+    title: 'Multi-Panel Displays',
+    path: 'https://www.xyzdisplays.com/Multi-Panel-Fabric-Displays-s/24732.htm',
+    imgFront:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/cb31030-3T.jpg?v-cache=1670494018',
+    imgBack:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/or59100-3T.jpg?v-cache=1670494018',
+  },
+  {
+    title: '20x20 Custom Booths',
+    path:
+      'https://www.xyzdisplays.com/20x20-Trade-Show-Displays-and-Booths-s/71.htm',
+    imgFront:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/or62540-3T.png?v-cache=1677232760',
+    imgBack:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/or10470-2T.jpg?v-cache=1692012374',
+  },
+  {
+    title: 'Tension Fabric Displays',
+    path: 'https://www.xyzdisplays.com/Slim-Tension-Fabric-Displays-s/171.htm',
+    imgFront:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/or34300-2T.png?v-cache=1684847070',
+    imgBack:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/ws03500-2T.jpg?v-cache=1670494018',
+  },
+  {
+    title: 'Banner Stands',
+    path: 'https://www.xyzdisplays.com/Banner-Stands-s/51.htm',
+    imgFront:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/or20050-2T.jpg?v-cache=1670494018',
+    imgBack:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/or35810-2T.jpg?v-cache=1677231856',
+  },
+  {
+    title: 'Outdoor Flags',
+    path: 'https://www.xyzdisplays.com/Outdoor-Promotional-Flags-s/185.htm',
+    imgFront:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/or29350-2T.png?v-cache=1670494018',
+    imgBack:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/or25425-2T.jpg?v-cache=1670494018',
+  },
+  {
+    title: 'Backlit Hanging Signs',
+    path:
+      'https://www.xyzdisplays.com/Illuminated-Backlit-Trade-Show-Hanging-Banner-Display-s/1671.htm',
+    imgFront:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/mk91520-2T.jpg?v-cache=1670494018',
+    imgBack:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/or46710-2T.jpg?v-cache=1670494018',
+  },
+  {
+    title: 'Backlit Counters',
+    path: 'https://www.xyzdisplays.com/Backlit-Counters-s/10285.htm',
+    imgFront:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/mk90780-2T.jpg?v-cache=1670494018',
+    imgBack:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/or10300-2T.jpg?v-cache=1670494018',
+  },
+  {
+    title: 'Four-Sided Walls',
+    path:
+      'https://www.xyzdisplays.com/4-Sided-TensionFabric-Displays-s/6041.htm',
+    imgFront:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/MK55160-2T.jpg?v-cache=1674129296',
+    imgBack:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/bn00032-3T.png?v-cache=1671628951',
+  },
+  {
+    title: 'Merchandising Displays',
+    path: 'https://www.xyzdisplays.com/Merchandiser-Exhibits-s/4011.htm',
+    imgFront:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/MK23050-3T.jpg?v-cache=1670494018',
+    imgBack:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/or7300-6T.jpg?v-cache=1690449700',
+  },
+  {
+    title: 'Modular Exhibits',
+    path: 'https://www.xyzdisplays.com/Hybrid-Modular-Exhibits-s/25931.htm',
+    imgFront:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/or26240-2T.png?v-cache=1670494018',
+    imgBack:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/na10140-3T.jpg?v-cache=1673345237',
+  },
+  {
+    title: 'Tabletop Displays',
+    path: 'https://www.xyzdisplays.com/Tabletop-Displays-s/60.htm',
+    imgFront:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/or56030-2T.jpg?v-cache=1670494018',
+    imgBack:
+      'https://www.xyzdisplays.com/v/vspfiles/photos/ws03400-2T.jpg?v-cache=1670494018',
+  },
+]
+
+$(document).ready(function () {
+  // generate html dynamically DOM with img
+  $.each($carouselData, function (index, item) {
+    $slideTrack = $('<div>', {
+      class: 'slide',
+    }).appendTo($('.xyzSlideshow'))
+    $thelink = $('<a>', {
+      href: item.path,
+      class: 'title-link-slider',
+      id: item.id,
+    }).appendTo($slideTrack)
+    $imgwrap = $('<div>', {
+      class: 'hover-switch',
+    }).appendTo($thelink)
+    $theimg = $('<img>', {
+      src: item.imgFront,
+      class: 'img-responsive-main-page-slider',
+    }).appendTo($imgwrap)
+    $imgOnHover = $('<img>', {
+      src: item.imgBack,
+      class: 'img-responsive-main-page-slider imgOnHover',
+    }).insertAfter($theimg)
+    $thename = $('<div>', {
+      text: item.title,
+      class: 'main-page-title-slider',
+    }).appendTo($thelink)
+  })
+})
+// flickity plugin - https://flickity.metafizzy.co/
+document.addEventListener('DOMContentLoaded', function (event) {
+  //
+  //   Variables
+  //
+  //////////////////////////////////////////////////////////////////////
+
+  // Play with this value to change the speed
+  let tickerSpeed = 2
+
+  let flickity = null
+  let isPaused = false
+  const slideshowEl = document.querySelector('.xyzSlideshow')
+  // new
+  let previousTime = 0
+  const frameRate = 60 // Set your desired frame rate here, e.g., 30 for 30 FPS
+  const frameDuration = 1000 / frameRate
+  // new end
+
+  //
+  //   Functions
+  //
+  //////////////////////////////////////////////////////////////////////
+
+  const update = (currentTime) => {
+    const deltaTime = currentTime - previousTime
+    if (deltaTime >= frameDuration) {
+      // Update your animation here
+      if (isPaused) return
+      if (flickity.slides) {
+        flickity.x = (flickity.x - tickerSpeed) % flickity.slideableWidth
+        flickity.selectedIndex = flickity.dragEndRestingSelect()
+        flickity.updateSelectedSlide()
+        flickity.settle(flickity.x)
+      }
+    // Reset the previous time to the current time
+    previousTime = currentTime
+    }
+
+
+    window.requestAnimationFrame(update)
+
+  }
+
+  const pause = () => {
+    isPaused = true
+  }
+
+  const play = () => {
+    if (isPaused) {
+      isPaused = false
+      window.requestAnimationFrame(update)
+    }
+  }
+
+  //
+  //   Create Flickity
+  //
+  //////////////////////////////////////////////////////////////////////
+
+  flickity = new Flickity(slideshowEl, {
+    autoPlay: false,
+    prevNextButtons: true,
+    pageDots: false,
+    draggable: true,
+    wrapAround: true, // infinite scrolling
+    selectedAttraction: 0.015,
+    friction: 0.25,
+    groupCells: 2,
+    //   groupCells: '80%',
+    fullscreen: true,
+    lazyLoad: 1,
+    adaptiveHeight: true,
+    setGallerySize: true,
+    contain: true,
+    freeScroll: true, // Enables content to be freely scrolled and flicked without aligning cells to an end position.
+  })
+  flickity.x = 0
+
+  //
+  //   Add Event Listeners
+  //
+  //////////////////////////////////////////////////////////////////////
+
+  slideshowEl.addEventListener('mouseenter', pause, false)
+  slideshowEl.addEventListener('focusin', pause, false)
+  slideshowEl.addEventListener('mouseleave', play, false)
+  slideshowEl.addEventListener('focusout', play, false)
+
+  flickity.on('dragStart', () => {
+    isPaused = true
+  })
+
+  //
+  //   Start Ticker
+  //
+  //////////////////////////////////////////////////////////////////////
+
+  update()
+})
